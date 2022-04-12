856e7890386aac88d401e54f79cf9513b14ab717
hnrqblck <hnrqblck@gmail.com>
2022-04-12 12:21:21 -0300

    [JS-170] fix: cors origin

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(express.json({ limit: '25mb' }));
 app.use(express.urlencoded({ limit: '25mb', extended: true }));
 // app.use(cors());
 app.use(cors({
-    origin: '*'
+    origin: 'https://linke-st.herokuapp.com'
 }));
 
 router.get('/', cors(), async (req, res) => {
