7a01b46c5d375a15b4608bbc299c8801487349e0
hnrqblck <hnrqblck@gmail.com>
2022-04-12 15:36:42 -0300

    [JS-170] change express.json

--- src/app.js
+++ src/app.js
@@ -8,10 +8,10 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
-const router = express.Router();
 
 app.use(express.json({ limit: '25mb' }));
 app.use(express.urlencoded({ limit: '25mb', extended: true }));
+const router = express.Router();
 // app.use(cors({
 //     origin: 'https://linke-st.herokuapp.com'
 // }));
