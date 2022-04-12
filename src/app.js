7a3363e6c0f78f747b6aca96e747be9c92c3368b
hnrqblck <hnrqblck@gmail.com>
2022-04-12 12:19:05 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -11,8 +11,10 @@ const router = express.Router();
 
 app.use(express.json({ limit: '25mb' }));
 app.use(express.urlencoded({ limit: '25mb', extended: true }));
-app.use(cors());
-router.options('/score', cors())
+// app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 
 router.get('/', cors(), async (req, res) => {
     res.json('This is working');
