e31f558d42e544b71a812246aa9317762028c8ac
hnrqblck <hnrqblck@gmail.com>
2022-04-12 15:29:01 -0300

    [JS-170] fix: change cors position

--- src/app.js
+++ src/app.js
@@ -7,11 +7,11 @@ const dt = require('./globaldata.js').data;
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
 const router = express.Router();
 
 app.use(express.json({ limit: '25mb' }));
 app.use(express.urlencoded({ limit: '25mb', extended: true }));
-app.use(cors());
 // app.use(cors({
 //     origin: 'https://linke-st.herokuapp.com'
 // }));
