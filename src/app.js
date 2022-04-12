a1ab86b3253e090cdbe2289dda41487d9326432c
hnrqblck <hnrqblck@gmail.com>
2022-04-12 14:58:46 -0300

    cors w deploy

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const router = express.Router();
 
 app.use(express.json({ limit: '25mb' }));
 app.use(express.urlencoded({ limit: '25mb', extended: true }));
-// app.use(cors());
+app.use(cors());
 app.use(cors({
     origin: 'https://linke-st.herokuapp.com'
 }));
