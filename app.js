2253e74cb657c1345939707d9fa2ca61335ad360
hnrqblck <hnrqblck@gmail.com>
2022-04-12 16:47:00 -0300

    cors change origin

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ require('dotenv').config();
 
 const app = express();
 const port = 3001;
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: 'https://linke-st.herokuapp.com'
+}));
 const router = express.Router();
 
 app.use(express.json({ limit: '25mb' }));
 app.use(express.urlencoded({ limit: '25mb', extended: true }));
-// app.use(cors({
-//     origin: 'https://linke-st.herokuapp.com'
-// }));
 
 app.get('/', cors(), async (req, res) => {
     res.json('This is working');
